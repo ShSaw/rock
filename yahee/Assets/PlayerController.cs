b141f8bacffd007f65b1753f86de39f5637c6029
Tyler Iams <tyleriams@Tylers-MacBook-Pro.local>
2019-06-25 14:26:14 -0700

    fixes

---
+++ yahee/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool paper_bool;
+    bool rock_bool;
+    bool scissors_bool;
+    private Animator animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("2"))
+        {
+            paper_bool = true;
+        }
+        if (paper_bool == true)
+        {
+            animation.Play("leftpaper");
+        }
+        if (Input.GetKeyDown("1"))
+        {
+            rock_bool = true;
+        }
+        if (rock_bool == true)
+        {
+            animation.Play("rock left");
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            scissors_bool = true;
+        }
+        if (scissors_bool == true)
+        {
+            animation.Play("leftscissors");
+        }
+    }
+}
---
+++ yahee/Assets/cpu_script_real.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cpu_script_real : MonoBehaviour
+{
+    System.Random r;
+    int num;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = new System.Random();
+        num = r.Next(1, 4);
+        anim = GetComponent<Animator>();
+        StartCoroutine(Wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+
+        if (num == 1)
+        {
+            anim.SetBool("rock", true);
+            anim.Play("rock");
+        }
+        else if (num == 2)
+        {
+            anim.SetBool("paper", true);
+        }
+        else
+        {
+            anim.SetBool("scissors", true);
+        }
+    }
+}
